34f451ca7ce0cffe41cc82c0157764907cbff878
sebastian dev <sebastianglg64@gmail.com>
2025-06-16 23:49:34 -0500

    agregando recomendacion antes de desplegar la aplicacion con un dominio real

--- app/sessions.server.ts
+++ app/sessions.server.ts
@@ -20,6 +20,7 @@ const { getSession, commitSession, destroySession } =
                 name: "__session",
 
                 // all of these are optional
+                // todo - ingresar el dominio donde se va a desplegar la app
                 domain: "localhost",
                 // Expires can also be set (although maxAge overrides it when used in combination).
                 // Note that this method is NOT recommended as `new Date` creates only one date on each server deployment, not a dynamic date in the future!
